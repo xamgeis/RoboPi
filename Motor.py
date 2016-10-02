c93784fc749171a587c9c69a316853287f96fedd
max <max.t.geislinger.com>
2016-10-02 00:16:51 +0000

    motor.py works now

--- Motor.py
+++ Motor.py
@@ -1,31 +1,60 @@
-
-import Rpi.gpio as gpio
+import RPi.GPIO as gpio
 from time import sleep
 
 gpio.setmode(gpio.BOARD)
 
-motor1a = 16
-motor1b = 18
-motor1e = 22
+motorRa = 16
+motorRb = 18
+motorRe = 22
 
+motorLa = 36
+motorLb = 38
+motorLe = 40
 
-gpio.setup(motor1a, gpio.OUT)
-gpio.setup(motor1b, gpio.OUT)
-gpio.setup(motor1e, gpio.OUT)
+gpio.setwarnings(False)
 
-print "Foward"
-gpio.output(motor1a, gpio.HIGH)
-gpio.output(motor1b, gpio.LOW)
-gpio.output(motor1e, gpio.HIGH)
+gpio.setup(motorRa, gpio.OUT)
+gpio.setup(motorRb, gpio.OUT)
+gpio.setup(motorRe, gpio.OUT)
 
+gpio.setup(motorLa, gpio.OUT)
+gpio.setup(motorLb, gpio.OUT)
+gpio.setup(motorLe, gpio.OUT)
+
+def front():
+	gpio.output(motorRa, gpio.HIGH)
+	gpio.output(motorRb, gpio.LOW)
+	gpio.output(motorRe, gpio.HIGH)
+
+	gpio.output(motorLa, gpio.HIGH)
+	gpio.output(motorLb, gpio.LOW)
+	gpio.output(motorLe, gpio.HIGH)
+
+def back():
+	gpio.output(motorRa, gpio.LOW)
+	gpio.output(motorRb, gpio.HIGH)
+	gpio.output(motorRe, gpio.HIGH)
+
+	gpio.output(motorLa, gpio.LOW)
+	gpio.output(motorLb, gpio.HIGH)
+	gpio.output(motorLe, gpio.HIGH)
+
+def stop():
+	gpio.output(motorRe, gpio.LOW)
+	gpio.output(motorLe, gpio.LOW)
+
+print "Fowards"
+front()
 sleep(2)
 
 print "Backwards"
-gpio.output(motor1a, gpio.LOW)
-gpio.output(motor1b, gpio.LOW)
-gpio.output(motor1e, gpio.HIGH)
+back()
+sleep(2)
+front()
+
+sleep(2)
 
 print "stop motor"
-gpio.output(motor1e, gpio.HIGH)
+stop()
 
 gpio.cleanup()
